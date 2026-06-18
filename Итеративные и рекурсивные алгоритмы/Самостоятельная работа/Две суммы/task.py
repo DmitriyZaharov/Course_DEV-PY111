bf51e89df05ddba4ca930b299c5a2a4eec3f8016
Dmitriy Zaharov <zaharov_dv@radar-mms.com>
2026-06-18 12:46:16 +0300

    Выполнена самостоятельная работа

--- Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Две суммы/task.py
+++ Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Две суммы/task.py
@@ -2,4 +2,26 @@ from typing import List
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
-    ...
+    """
+    Находит индексы двух чисел, которые в сумме дают target.
+
+    :param nums: Список целых чисел
+    :param target: Целевая сумма
+    :return: Список из двух индексов
+    """
+    # Словарь для хранения пар {число: его_индекс}
+    seen = {}
+
+    for index, num in enumerate(nums):
+        # Ищем число, которого не хватает до target
+        complement = target - num
+
+        # Если такое число уже встречалось, возвращаем его индекс и текущий индекс
+        if complement in seen:
+            return [seen[complement], index]
+
+        # Если не встречалось, сохраняем текущее число и его индекс
+        seen[num] = index
+
+    # По условию задачи решение всегда существует, поэтому сюда код не дойдет
+    raise ValueError("Решение не найдено")
--- Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Ряды Тейлора/task.py
+++ Итеративные и рекурсивные алгоритмы/Самостоятельная работа/Ряды Тейлора/task.py
@@ -1,7 +1,6 @@
 from typing import Union
 from itertools import count
-from math import factorial
-
+import math
 
 DELTA = 0.000001
 
@@ -13,4 +12,30 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x значение в радианах
     :return: значение sin(x)
     """
-    ...  # TODO вычислить sin(x) с помощью разложения сумму бесконечного ряда
+    # Приводим x к диапазону [-pi, pi] для точности и быстрой сходимости
+    x = math.fmod(x, 2 * math.pi)
+    if x > math.pi:
+        x -= 2 * math.pi
+    elif x < -math.pi:
+        x += 2 * math.pi
+
+    # Инициализация для первого члена ряда (n = 0)
+    # Первый член равен x, так как: ((-1)**0 * x**(2*0 + 1)) / (2*0 + 1)! = x / 1 = x
+    current_term = x
+    sin_sum = current_term
+
+    # Начинаем со следующего шага (n = 1)
+    for n in count(1):
+        # Коэффициент перехода от предыдущего члена ряда к следующему:
+        # -x^2 / ((2n) * (2n + 1))
+        multiplier = -(x ** 2) / ((2 * n) * (2 * n + 1))
+        current_term *= multiplier
+
+        sin_sum += current_term
+
+        # Если модуль текущего члена стал меньше DELTA, останавливаемся
+        if abs(current_term) < DELTA:
+            break
+
+    return sin_sum
+
