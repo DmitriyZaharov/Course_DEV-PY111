0b5dcc341845fd65d35293ac590837f4be81e542
Dmitriy Zaharov <zaharov_dv@radar-mms.com>
2026-06-18 12:25:15 +0300

    Выполнены практические задания

--- Итеративные и рекурсивные алгоритмы/Практические задания/Рекурсивный бинарный поиск/task.py
+++ Итеративные и рекурсивные алгоритмы/Практические задания/Рекурсивный бинарный поиск/task.py
@@ -1,6 +1,5 @@
 from typing import Sequence
 
-
 def binary_search(
         value: int, seq: Sequence[int],
         left_border: int = 0, right_border: int = None
@@ -16,4 +15,26 @@ def binary_search(
     :raise: ValueError если элемента нет в массиве
     :return: Индекс элемента в массиве
     """
-    ...  # TODO реализовать алгоритм бинарного поиска
+
+    if right_border is None:
+        right_border = len(seq) - 1
+
+    # Базовый случай рекурсии
+    if left_border > right_border:
+        raise ValueError(f"Элемент {value} отсутствует в массиве")
+
+    mid = left_border + (right_border - left_border) // 2
+
+    if seq[mid] == value:
+        # Если это самый первый элемент массива или элемент слева меньше искомого,
+        # значит mid — это действительно первое вхождение.
+        if mid == 0 or seq[mid - 1] < value:
+            return mid
+        # Иначе продолжаем искать левее, чтобы найти самое первое вхождение
+        return binary_search(value, seq, left_border, mid - 1)
+
+    if seq[mid] > value:
+        return binary_search(value, seq, left_border, mid - 1)
+
+    return binary_search(value, seq, mid + 1, right_border)
+
